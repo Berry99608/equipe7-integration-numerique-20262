0ec6b705eace27070e3ce7bdaa17510c1425fd52
kevin-noah <kevinnoahy@icloud.com>
2026-06-02 15:59:51 -0400

    feat : ajout de la solution integration par rectangle

--- integration_rectangles.py
+++ integration_rectangles.py
@@ -11,3 +11,12 @@ def solution_analytique(p1, p2, p3, p4, a, b):
     def antiderivee(x):
         return p1 * x + p2 * x**2 / 2 + p3 * x**3 / 3 + p4 * x**4 / 4
     return antiderivee(b) - antiderivee(a)
+
+def rectangles_python(p1, p2, p3, p4, a, b, n):
+    h = (b - a) / n
+    total = 0.0
+    for i in range(n):
+        x_mid = a + (i + 0.5) * h  # Point milieu du segment i
+        total += evaluer_poly(x_mid, p1, p2, p3, p4)
+    return total * h
+
