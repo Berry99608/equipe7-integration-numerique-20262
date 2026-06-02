9c2c3c9204534fc3e243f2d465a9d4c2007093a6
kevin-noah <kevinnoahy@icloud.com>
2026-06-02 15:57:15 -0400

    feat : ajout de la solution integration analytique

--- integration_rectangles.py
+++ integration_rectangles.py
@@ -5,3 +5,9 @@ import timeit
 def evaluer_poly(x, p1, p2, p3, p4):
     # Fonctionne avec un scalaire ou un tableau NumPy
     return p1 + p2 * x + p3 * x**2 + p4 * x**3
+
+def solution_analytique(p1, p2, p3, p4, a, b):
+    # Antidérivée de f(x) : F(x) = p1*x + p2*x²/2 + p3*x³/3 + p4*x⁴/4
+    def antiderivee(x):
+        return p1 * x + p2 * x**2 / 2 + p3 * x**3 / 3 + p4 * x**4 / 4
+    return antiderivee(b) - antiderivee(a)
