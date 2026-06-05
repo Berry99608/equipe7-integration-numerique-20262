4c504af7fbd2793d00b597c3a67d802a5a430857
syndiajean <syndiajean1@gmail.com>
2026-06-04 23:29:03 -0400

    feat : ajout des imports + création des fonctions scipy_trapeze et scipy_simpson

---
+++ visualisation.py
@@ -0,0 +1,14 @@
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from scipy import integrate
+from integration_rectangles import evaluer_poly
+
+def scipy_trapezes(p1, p2, p3, p4, a, b, n):
+    x = np.linspace(a, b, n+1)
+    return float(integrate.trapezoid(evaluer_poly(x, p1, p2, p3, p4), x))
+
+def scipy_simpson(p1, p2, p3, p4, a, b, n):
+    x = np.linspace(a, b, n+1)
+    return float(integrate.simpson(evaluer_poly(x, p1, p2, p3, p4), x))
