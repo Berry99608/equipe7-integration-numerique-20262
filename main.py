cd77a37ca2ecebbdf11ebeaec59d87b936b217dd
syndiajean <syndiajean1@gmail.com>
2026-06-05 04:18:24 -0400

    feat : Ajout des import dans le main et du dictionnaire des fonctions

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+"""
+main.py
+=======
+MGA802 — Mini-Projet B : Intégration numérique
+"""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from integration_rectangles import solution_analytique, rectangles_python, rectangles_numpy, calculer_erreur
+from integration_avancee import trapezes_numpy, trapezes_python, simpson_numpy, simpson_python
+from visualisation import scipy_trapezes, scipy_simpson, graphique_convergence, graphique_temps, graphique_erreur_methodes
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+Liste_n = [5, 10, 20, 50, 100, 200, 500, 1000]
+n_ref = 100
+repetitions = 100
 
+FONCTIONS = {"Rectangle Python": rectangles_python,
+             "Rectangle NumPy": rectangles_numpy,
+             "Trapeze Python": trapezes_python,
+             "Trapeze NumPy": trapezes_numpy,
+             "Simpson Python": simpson_python,
+             "Simpson NumPy": simpson_python,
+             "Scipy Trapeze": scipy_trapezes,
+             "Scipy Simpson": scipy_simpson,}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
